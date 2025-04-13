a08b5f51dd7dce0d65dd0a6f8bd5e8eeaae0fa56
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 06:31:42 +0530

    student course

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
-const shoproutes = require('./routes/shop')
 
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 
+// Middleware to parse JSON (optional, but good practice)
 app.use(express.json());
 
-
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next(); 
+// Home route
+app.get('/', (req, res) => {
+    res.send("Welcome to the Student & Course Portal API!");
 });
 
+// Use Routes
+app.use('/students', studentRoutes);
+app.use('/courses', courseRoutes);
 
-app.use(shoproutes);
+// 404 handler
+app.use('*', (req, res) => {
+    res.status(404).send("Page not found");
+});
 
-
-app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000");
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
