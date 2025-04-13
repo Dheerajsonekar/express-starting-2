720a4ffde3a7186048fe44914c2f3a5dd94af250
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 06:16:16 +0530

    library management

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/products', (req, res) => {
-    res.send('<h1>Here is the list of all products.</h1>');
+router.get('/books', (req, res) => {
+    res.send('<h1>Here is the list of all books.</h1>');
 });
 
-router.post('/products', (req, res) => {
-    res.send('<h1>A new product has been added.</h1>');
+router.post('/books', (req, res) => {
+    res.send('<h1>A new book has been added.</h1>');
 });
 
 
-router.get('/categories', (req, res) => {
-    res.send('<h1>Here is the list of all categories.</h1>');
-});
 
-router.post('/categories', (req, res) => {
-    res.send('<h1>A new category has been created.</h1>');
-});
 
 module.exports = router;
